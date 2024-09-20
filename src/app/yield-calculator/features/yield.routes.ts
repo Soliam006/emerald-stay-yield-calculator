0cc6f96da8a6bcaa5240c534ca68a1be58b0e9c6
Soliam006 <wsottoja7@alumnes.ub.edu>
2024-09-20 23:57:38 +0200

    First View, Undone

--- src/app/yield-calculator/features/yield.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { YieldCalculatorComponent } from './yield-calculator.component';
-
-export const YIELD_ROUTES: Routes = [
-  {
-    path: '',
-    component: YieldCalculatorComponent
-  }
-];
